0e80a6fe96eb1e93597df2f6fe8f2b517c1aad20
cjg <cjg@DESKTOP-HITIFPH>
2020-03-09 23:28:28 +0900

    1

---
+++ Netty/src/chap4/EchoServerV2Handler.java
@@ -0,0 +1,30 @@
+package chap4;
+
+import java.nio.charset.Charset;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoServerV2Handler extends ChannelInboundHandlerAdapter {
+	
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		ByteBuf readMessage = (ByteBuf)msg;
+		System.out.println("channelRead : " + readMessage.toString(Charset.defaultCharset()));
+		ctx.write(msg);
+	}
+	
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) {
+		System.out.println("channelReadComplete ¹ß»ý");
+		ctx.flush();
+	}
+	
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+}
